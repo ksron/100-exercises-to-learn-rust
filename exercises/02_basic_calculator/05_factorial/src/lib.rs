4774ce551dce4c5daa01836f3de47dbbc5effbe3
kimsron <sharon.kim@avikus.ai>
2024-05-23 17:09:06 +0900

    02 calculation done

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -9,6 +9,16 @@
 // `factorial(2)` to return `2`, and so on.
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
+fn factorial(x:u32) -> u32 {
+    if x == 0 {
+        1
+    } else if x <= 2 {
+        x
+    }
+    else {
+        x * factorial(x-1)
+    }
+}
 
 #[cfg(test)]
 mod tests {
